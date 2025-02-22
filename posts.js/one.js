68feb031fa19c138b76ceb4e70116fbd94d08141
tipusahil <tipusahil.ctg@gmail.com>
2025-02-23 00:27:35 +0600

    ol list container createeed & now will created li item list in ol element

--- posts.js/one.js
+++ posts.js/one.js
@@ -8,15 +8,31 @@ function loadAllPosts() {
 
 }
 
+
+/* 
+1. get the container element where i want to add the new elementss
+2.
+*/
+
+
 function displayAllPosts (usersAllPosts) {
 
 // console.log(usersAllPosts);
- const posts_main_container = document.getElementById('')
+ const posts_main_container = document.getElementById('all_posts_container');
 
 for(const user1post1 of usersAllPosts) {
-    // console.log(user1post1)
+    console.log(user1post1)
 
+ 
+    const div = document.createElement('div');
+    div.classList.add('post')
+    div.innerHTML = ` 
+    <h4> UserId-${user1post1.id} </h4>
+    <h5> post-title: ${user1post1.title} </h5>
+    <h5> Description: ${user1post1.body} </h5>
+     `;
 
+     posts_main_container.appendChild(div);
 }
 
 
