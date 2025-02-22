cd0b44986c4b3ec26e478dedeeb7dbada17c4c4c
tipusahil <tipusahil.ctg@gmail.com>
2025-02-22 21:50:00 +0600

    ol list container createeed & now will created li item list in ol element

--- js/user2.js
+++ js/user2.js
@@ -2,6 +2,22 @@
 
 function loadUsers2() {
     fetch('https://jsonplaceholder.typicode.com/users')//< ekan (semiclone (;) dil error dibe, )
-    .then(response => response.json())
-    .then(data => console.log(data))
+        .then(response => response.json())
+        .then(data => displayUsers2(data))
+}
+
+function displayUsers2(users) {
+
+    for (const user1 of users) {
+        //    console.log(user1);
+        // console.log(user1.email);
+        // console.log(user1.username);
+        console.log(user1.username);
+
+
+
+
+
+    }
+
 }
