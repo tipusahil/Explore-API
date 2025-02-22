0aeb5053994f159b1ce4c36ecf96dcd848ee30a9
tipusahil <tipusahil.ctg@gmail.com>
2025-02-22 20:44:53 +0600

    setup api folder

--- js/user2.js
+++ js/user2.js
@@ -3,5 +3,5 @@
 function loadUsers2() {
     fetch('https://jsonplaceholder.typicode.com/users')//< ekan (semiclone (;) dil error dibe, )
     .then(response => response.json())
-    .then(resp)
+    .then(data => console.log(data))
 }
