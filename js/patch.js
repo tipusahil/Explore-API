7e548e84422c8b5d9b7c391ff42cd40c8b51298e
tipusahil <tipusahil.ctg@gmail.com>
2025-02-23 10:46:46 +0600

    photos json

---
+++ js/patch.js
@@ -0,0 +1,11 @@
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PATCH',
+    body: JSON.stringify({
+      title: 'foo',
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
